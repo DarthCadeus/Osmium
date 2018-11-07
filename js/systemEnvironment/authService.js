8544031078cfba5a75f5bf2a9af26672cc1d77a7
Bingdong Li <lbdro2017@yandex.com>
2018-11-07 08:06:33 +0800

    added "remember me" support for app key and app id

--- js/systemEnvironment/authService.js
+++ js/systemEnvironment/authService.js
@@ -1,4 +1,12 @@
+const ls = new LocalStorageManager(false)
+
+
 function lcInit() {
+	if(ls.get("appKey") && ls.get("appId")) {
+		leancloudInit(ls.get("appId"), ls.get("appKey"));
+		logIn();
+		return;
+	}
 	const options_redundant = {
 		title: "link to a database!",
 		beforeClose: function(event, ui) {
@@ -17,17 +25,31 @@ function lcInit() {
 		"		<p>We do not supply a database because we respect your privacy. If you want to create your own, we are all for it! </p>",
 		"		<p>However, if you simply wish to try this out, it is okay to use our trial database. Simply click <button class='useDefault'>this button</button>!</p>",
 		"		<p>If you so wish, be warned that there is a limit to requests set by the provider. </p>",
-		"		<p>Id: <input class='appId' place='app id'><br/>Key: <input type='password' class='appKey' place='app key'><br/><button class='useCustom'>confirm</button></p>",
+		"		<p>Id: <input class='appId' place='app id'><br/>Key: <input type='password' class='appKey' place='app key'><br/><input type='checkbox' class='rememberMe'>remember me<br/><input type='checkbox' class='rememberMeTen'>remember me for ten days<br/><button class='useCustom'>confirm</button></p>",
 		"	</div>",
 		"</div>"
 	].join("\n"));
 	$("body").append(frame);
 	$(".authServicePopUp p .useDefault").click(function(){
+		if($(".authServicePopUp p .rememberMe").val()) {
+			ls.set("appId", "MDPFnMc2dUwuywGmueDNyOft-gzGzoHsz");
+			ls.set("appKey", "LSrD2F64K8NEnQftI8vkxEfT");
+		} else if ($(".authServicePopUp p .rememberMeTen").val()) {
+			ls.set("appId", "MDPFnMc2dUwuywGmueDNyOft-gzGzoHsz", 10);
+			ls.set("appKey", "LSrD2F64K8NEnQftI8vkxEfT", 10);
+		}
 		leancloudInit();
 		frame.remove();
 		logIn();
 	});
 	$(".authServicePopUp p .useCustom").click(function(){
+		if($(".authServicePopUp p .rememberMe").val()) {
+			ls.set("appId", $(".authServicePopUp p .appId").val());
+			ls.set("appKey", $(".authServicePopUp p .appKey").val());
+		} else if ($(".authServicePopUp p .rememberMeTen").val()) {
+			ls.set("appId", $(".authServicePopUp p .appId").val(), 10);
+			ls.set("appKey", $(".authServicePopUp p .appKey").val(), 10);
+		}
 		leancloudInit($(".authServicePopUp p .appId").val(), $(".authServicePopUp p .appKey").val());
 		frame.remove();
 		logIn();
--- js/utils.js
+++ js/utils.js
@@ -4,7 +4,7 @@ function randint (min, max) {
 function choice(list) {
     return list[randint(0, list.length-1)];
 }
-var random = {
+let random = {
     randint: randint,
     choice: choice
 }
@@ -28,3 +28,65 @@ function leancloudInit(id, key) {
         });
     }
 }
+
+class LocalStorageManager {
+    constructor(prioritizeCookies) {
+        this.prioritizeCookies = prioritizeCookies;
+    }
+}
+
+LocalStorageManager.prototype.set = function (key, value, exdays) {
+    if(exdays) {
+        this.setCookie(key, value, exdays);
+    } else {
+        window.localStorage.setItem(key, value);
+    }
+};
+
+LocalStorageManager.prototype.get = function (key) {
+    const ls = window.localStorage.getItem(key);
+    if(ls) return ls;
+    return this.getCookie(key);
+};
+
+LocalStorageManager.prototype.cancel = function (key) {
+    window.localStorage.removeItem(key);
+    this.deleteCookie(key);
+};
+
+LocalStorageManager.prototype.setCookie = function (key, value, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = key + "=" + value + ";" + expires + ";path=/";
+};
+
+LocalStorageManager.prototype.getCookie = function (key) {
+    let name = key + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+LocalStorageManager.prototype.renewCookie = function (key, renew) {
+    const step = renew || 30;
+    const value = this.getCookie(key);
+    if(value == "") return false;
+    this.setCookie(key, value, step);
+    return true;
+};
+
+LocalStorageManager.prototype.deleteCookie = function (key) {
+    const value = this.getCookie(key);
+    if(value == "") return false;
+    this.setCookie(key, value, -1);
+};
