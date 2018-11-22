bf4cdc81734585a5cabe737147d3b7977fcd53dc
Bingdong Li <lbdro2017@yandex.com>
2018-11-22 13:17:28 +0800

    Included test for App Api

--- js/AppSystem/AppApi.js
+++ js/AppSystem/AppApi.js
@@ -1,24 +1,12 @@
-window.addEventListener("message", receiveAppMessage);
+AppObject.prototype.getElement = function (a) {
+    // now the namespace is enforced
+    let res = $(this.el).find(a)[0];
+    res.parentElement = undefined;
+    return res;  // oh yes, just you try to get its parent
+};
 
-function receiveAppMessage(event) {
-    let message = event.message;
-    if (message.originator != "app") {
-        return;
-    }
-    if (AppOrigins.indexOf(event.origin) == -1) {
-        return;
-    }
-    if (message.command.purpose == "file") {
-        let target = window.open(message.src, message.name);
-        // target.postMessage(fs.search(message.purpose.criteria), message.src);
-        if (message.command.instruction == "insert") {
-            let content = message.command.content;
-            let result = confirm(`An app at domain ${message.src} with name ${message.name} is trying to create a new file!`);
-            if (result) {
-                target.postMessage(false, message.src);
-            } else {
-                
-            }
-        }
-    }
+function generate_api(id) {
+    return {
+        getElement: undefined;
+    };
 }
